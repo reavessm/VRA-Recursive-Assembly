cb0486c7e017b6f99060b8a918f8648de26e71a6
cskeen <skeen.c@gmail.com>
2017-10-29 21:19:13 -0400

    Adding rollaballpp

---
+++ Rollaballpp/Assets/scripts/MovePlayer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float speed;
+    public ParticleSystem victory_effect;
+    public ParticleSystem landing_effect;
+
+	public SphereCollider col;
+	public LayerMask groundLayers;
+
+	private Rigidbody rb;
+    private bool finished;
+
+	void Start() { // Called on the first frame the script is active in.
+		rb = GetComponent<Rigidbody> (); // This will grab the object's Rigidbody component, if such a component exists.
+        finished = false;
+		col = GetComponent<SphereCollider> ();
+	}
+
+    void Update() { // This updates before every frame.
+		if ((isOnGround()) && (Input.GetKeyDown (KeyCode.Space))) {
+			rb.AddForce (Vector3.up * 20, ForceMode.Impulse);
+		}
+		if ( transform.position.y < -125 ) {
+			SceneManager.LoadScene ("youlose");
+		}
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bullseye")
+        {
+            finished = true;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            victory_effect.Play ();
+			SceneManager.LoadScene ("endgame");
+        }
+        else if (col.gameObject.tag == "Platform")
+        {
+            landing_effect.Play ();
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            landing_effect.Stop ();
+        }
+    }
+
+    void FixedUpdate() { //This updates before physics calculations.
+		float movehorizontal = Input.GetAxis ("Horizontal");
+		float movevertical = Input.GetAxis ("Vertical");
+		float mod_speed;
+
+		Vector3 movement = new Vector3 (movehorizontal, 0.0f, movevertical);
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			mod_speed = speed + 50;
+		} else {
+			mod_speed = speed - 50;
+		}
+
+        if (finished == false)
+        {
+            rb.AddForce (movement * mod_speed);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+	}
+
+	private bool isOnGround() {
+		return Physics.CheckCapsule (col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * 0.9f, groundLayers);
+	}
+}
---
+++ Rollaballpp/Assets/scripts/MoveTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour {
+
+	public int range;
+	public float rate;
+
+	private int position;
+	bool direction;
+
+	// Use this for initialization
+	void Start () {
+		position = 0;
+		direction = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction) {
+			transform.Translate (Vector3.left * rate * Time.deltaTime);
+			position++;
+			if (position == range) {
+				direction = !direction;
+			}
+		} else {
+			transform.Translate (Vector3.right * rate * Time.deltaTime);
+			position--;
+			if (position == -range) {
+				direction = !direction;
+			}
+		}
+
+	}
+}
