6265de1c042780ecf4d16b200ddb7da4f62d0a6b
cskeen <skeen.c@gmail.com>
2017-11-19 21:33:09 -0500

    Added world.xml

---
+++ XMLBuilderWinForms/XMLBuilderWinForms/documentHandler.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using XMLBuilder.Models;
+
+namespace XMLBuilderWinForms
+{
+    class documentHandler
+    {
+        private XDocument xmlDOM;
+
+        public documentHandler()
+        {
+            xmlDOM = new XDocument();
+            buildRootNode();
+        }
+
+        public documentHandler(string fileName)
+        {
+            try
+            {
+                xmlDOM = XDocument.Load(fileName);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Specified file cannot be read.", "fileName");
+            }
+        }
+
+        public string getFullString()
+        {
+            return xmlDOM.ToString();
+        }
+
+        public IEnumerable<XElement> getAllSubelements()
+        {
+            IEnumerable<XElement> subelements = xmlDOM.Root.Elements("parent").Elements("parent");
+            return subelements;
+        }
+
+        //private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        //{
+        //    XmlNode xNode;
+        //    TreeNode tNode;
+        //    XmlNodeList nodeList;
+        //    int i;
+
+        //    // Loop through the XML nodes until the leaf is reached.
+        //    // Add the nodes to the TreeView during the looping process.
+        //    if (inXmlNode.HasChildNodes)
+        //    {
+        //        nodeList = inXmlNode.ChildNodes;
+        //        for (i = 0; i <= nodeList.Count - 1; i++)
+        //        {
+        //            xNode = inXmlNode.ChildNodes[i];
+        //            inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+        //            tNode = inTreeNode.Nodes[i];
+        //            AddNode(xNode, tNode);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // Here you need to pull the data from the XmlNode based on the
+        //        // type of node, whether attribute values are required, and so forth.
+        //        inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+        //    }
+
+        //}
+
+        public void buildRootNode()
+        {
+
+        }
+    }
+}
---
+++ XMLBuilderWinForms/XMLBuilderWinForms/mainWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using XMLBuilder;
+
+namespace XMLBuilderWinForms
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void XMLTreeViewer_Load(object sender, EventArgs e)
+        {
+            KVTagBox.Text = Application.StartupPath + "\\sample.xml";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openCommand_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                documentHandler dh = new documentHandler("sample.xml");
+                IEnumerable<XElement> parents = dh.getAllSubelements();
+                string output = "";
+
+                foreach(XElement parent in parents)
+                {
+                    output += parent.ToString();
+                }
+
+
+                MessageBox.Show(output);
+                //XMLTreeViewer.Nodes.Add(new TreeNode(dom.DocumentElement.Name));
+                //TreeNode tNode = new TreeNode();
+                //tNode = XMLTreeViewer.Nodes[0];
+
+                //AddNode(dom.DocumentElement, tNode);
+                //XMLTreeViewer.ExpandAll();
+            }
+            catch (XmlException xmlEx)
+            {
+                MessageBox.Show(xmlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
