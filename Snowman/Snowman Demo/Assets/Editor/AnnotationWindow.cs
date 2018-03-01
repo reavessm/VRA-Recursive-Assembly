cce967b214b8529ea81ebebc3574e2cd5b818e0e
Christopher Skeen <skeen.c@gmail.com>
2018-02-28 19:44:49 -0500

    Adding Metadata.cs & Annotation Stub

---
+++ Snowman/Snowman Demo/Assets/Editor/AnnotationWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnnotationWindow : EditorWindow {
+	/// <summary>
+	/// OnGUI is called for rendering and handling GUI events.
+	/// This function can be called multiple times per frame (one call per event).
+	/// </summary>
+	void OnGUI()
+	{
+		// Window Code Goes Here
+			
+	}
+
+	[MenuItem ("Window/Annotate Scene")]
+	public static void ShowWindow() {
+		EditorWindow.GetWindow(typeof(AnnotationWindow));
+	}
+}
---
+++ Snowman/Snowman Demo/Assets/Scripts/Metadata.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Metadata : MonoBehaviour {
+	public string kvtagstring;
+
+	private SortedDictionary<string, string> kvtags;
+
+	/// <summary>
+	/// Called when the script is loaded or a value is changed in the
+	/// inspector (Called in the editor only).
+	/// </summary>
+	void OnValidate() {
+		updateTags();
+	}
+
+	private void updateTags() {
+		try {
+			if (!String.IsNullOrEmpty(kvtagstring)) {
+				if (kvtagstring.Contains(":") && !kvtagstring.EndsWith(":")) {
+					string[] outer = kvtagstring.Split(';');
+					foreach (string tag in outer) {
+						string[] inner = tag.Split(':');
+						kvtags.Add(inner[0], inner[1]);
+					}
+				}
+			}	
+		}
+		catch (Exception e) {
+			Debug.Log(e.Message);
+		}
+	}
+
+	public void setTags(string raw_tag) {
+		kvtagstring = raw_tag;
+		updateTags();
+	}
+
+	public SortedDictionary<string, string> getTags() {
+		return kvtags;
+	}
+
+	public int getOrder() {
+		int temp = -1;
+		Int32.TryParse(kvtags["order"], out temp);
+		return temp;
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
