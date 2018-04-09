669fdf62907ac78f0b364e5f7019e0aceea25cb6
Christopher Skeen <skeen.c@gmail.com>
2018-04-09 16:06:26 -0400

    Cleaning up the repo, moving the project out of the Snowman folder.

--- dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(MAX) NULL, 
-    [Date Added] DATETIME NULL, 
-    [Fastest Time] TIME NULL, 
-    [Trash] BIT NULL
-)
