bc4067db7eeeb242dc31a2ad8a23678235bef89a
freefire137 <ianawfinch@gmail.com>
2018-04-23 17:48:07 -0400

    globals.cs

---
+++ BOEING/Demo/Assets/Scripts/Globals.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour {
+	//not sure entirely what to put in here, not implementing yet either
+	public int SnappingDistance;			//controllergrabobject.cs
+	public int TeleportRange;				//teleport.cs
+	public vector GhostOffset;				//deploy.cs
+	public vector PartsOffset;				//deploy.cs why are these vectors? -if
+	public int AutoAssemblySpeed;			//scenesetter.cs
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
