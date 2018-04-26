4867b60cb3b08f0c9070cd4237b51ddc8e0de94e
Stephen Reaves <reavessm@email.sc.edu>
2018-04-25 20:32:41 -0400

    :beers: An attempt was made to consolidate public variables

---
+++ BOEING/Demo/Assets/Editor/GlobalVariables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+public class GlobalVariables : EditorWindow {
+
+  public static float ghostOffset_ = 10f;
+
+	[MenuItem ("Window/Set Global Variables")]
+	public static void ShowWindow() {
+		EditorWindow.GetWindow(typeof(AnnotationWindow), true, "Set Global Variables");
+	}
+
+	/// <summary>
+	/// OnGUI is called for rendering and handling GUI events.
+	/// This function can be called multiple times per frame (one call per event).
+	/// </summary>
+	void OnGUI()
+	{
+		GUILayout.Label("Base Settings", EditorStyles.boldLable);
+
+    ghostOffset_ = EditorGUILayout.Slider("Slider", ghostOffset_, 0, 20);
+	}
+
+}
